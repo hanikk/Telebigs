cff92238ef2032060261327637fd4bde0121d4c4
hanikk <hanikarim77@gmail.com>
2016-02-12 16:40:57 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -236,7 +236,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users ={183427353},--Sudo users
+    sudo_users ={149516204},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
